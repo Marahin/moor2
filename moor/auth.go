9fc7a86cd82465974aa48c027d29bfb5585720f2
marahin <me@marahin.pl>
2020-03-04 14:53:48 +0100

    On token failure, log both tokens

--- moor/auth.go
+++ moor/auth.go
@@ -13,7 +13,7 @@ func AuthorizeByToken(w *http.ResponseWriter, r *http.Request) error {
 	if !ok || len(key[0]) < 1 || key[0] != AUTH_TOKEN {
 		err := errors.New("URL parameter 'token' is invalid")
 
-		log.WithFields(log.Fields{"token": key}).Error(err)
+		log.WithFields(log.Fields{"token": key, "expected token": AUTH_TOKEN}).Error(err)
 		http.Error(*w, err.Error(), http.StatusUnauthorized)
 
 		return err
